d7cc6afe73540295880aa3d014ae9cd75c1e5c48
francisDYL <leroifrancis56@gmail.com>
2025-09-26 23:18:30 -0400

    adding unit tests and implementation

--- src/main/dueDateCalculator.ts
+++ src/main/dueDateCalculator.ts
@@ -1,10 +1,99 @@
+/**
+ * Calculator that compute due date of an issue on submission date and turnaround time
+ * Week-end are not working days and we do not consider holidays for computation
+ * We assume that submission date is already in a valid format
+ */
 export class DueDateCalculator {
 
     private dayStart: number;
     private dayEnd: number;
+    private shiftLength: number;
 
+    /**
+     * take in parameter working hour start and end in the 24H format
+    */
     constructor(_dayStart: number, _dayEnd: number) {
         this.dayStart = _dayStart;
         this.dayEnd = _dayEnd;
+        this.shiftLength = _dayEnd - _dayStart;
+    }
+
+    computeDueDate(submisionDate: string, tournaround: number): string {
+
+        // validate that date and touraround are valid input
+        const date: Date = new Date(submisionDate);
+        const dayOfWeek = date.getDay();
+        const submissionHour = date.getHours();
+
+        if(!this.isValidSubmissionDate(dayOfWeek,submissionHour)) throw new Error(`Submission date can only be on a working day and between wworking hours`);
+
+        if(tournaround <= 0) throw new Error (`Turnaround time can't be negative or null`);
+
+        // compute due date
+        while (tournaround > 0) {
+
+            // get remaining work time in hour
+            const remaining = (this.dayEnd - date.getHours()) - date.getMinutes()/60;
+
+            //if remaining time is enough
+            if(remaining > tournaround) {
+                date.setTime(date.getTime() + tournaround * 3600000);
+                tournaround = 0;
+            } else {
+            //verify is touraround time is more than a working day, then move day by day at the same time    
+                const days = Math.floor(tournaround / this.shiftLength);
+                if (days > 0) {
+                    for(let i =0; i < days; i++) {
+                      this.addOneDayOnDate(date);
+                    }
+                    tournaround -= days * this.shiftLength;
+                } else { // touraround time is less that a working day, then move to the next working day at 9 AM
+                    this.addOneDayOnDate(date);
+                    date.setHours(this.dayStart,0,0,0);
+                    tournaround -= remaining;
+                }
+                
+            }
+        }
+
+        //return the value
+        return this.formatDateTime(date);
+
+    }
+
+    //help validating that the submission date is correct
+    private isValidSubmissionDate(day: number, hour: number): boolean {
+        if( day === 0 || day === 6 || !(this.dayStart <= hour && hour < this.dayEnd)) 
+            return false;
+        return true;
+    }
+
+    /**
+     * format date to string
+     * @param date 
+     * @returns 
+     */
+    private formatDateTime(date: Date): string {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+
+        return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+    }
+
+    /**
+     * add one day on a date, and if the next is week-end then continue untill start of the week
+     * @param date 
+     */
+    private addOneDayOnDate(date: Date) {
+        date.setDate(date.getDate() + 1);
+        if(date.getDay() == 0) {
+            date.setDate(date.getDate() + 1);
+        } else if(date.getDay() == 6) {
+            date.setDate(date.getDate() + 2);
+        }
     }
 }
--- src/tests/dueDateCalculator.test.ts
+++ src/tests/dueDateCalculator.test.ts
@@ -1,6 +1,46 @@
 import {DueDateCalculator} from '../main/dueDateCalculator';
 
-test('dump test that do nothing', () => {
-    const calculator = new DueDateCalculator(9,17);
-    expect(calculator).toBeDefined()
+
+const calculator = new DueDateCalculator(9,17);
+
+test('Should throw exception when submission time is invalid', () => {
+    const call = () => { calculator.computeDueDate("2025-09-26T20:30:00",4); };
+    expect(call).toThrow(Error);
+});
+
+test('Should throw exception when touraround time is invalid', () => {
+    const call = () => { calculator.computeDueDate("2025-09-26T09:30:00",-2); };
+    expect(call).toThrow(Error)
+});
+
+test('tournaround time is less than the remaining time in the day', () => {
+    expect(calculator.computeDueDate('2025-09-26T10:30:00',4)).toEqual('2025-09-26T14:30:00');
+});
+
+test('tournaround time is greather than the remaining time in the day but still in working weeks', () => {
+    expect(calculator.computeDueDate('2025-09-25T09:30:00',10)).toEqual('2025-09-26T11:30:00');
+});
+
+test('touraround time is exactly one days of work and is greather than remaining time in the days',  () => {
+    expect(calculator.computeDueDate('2025-09-25T09:30:00',8)).toEqual('2025-09-26T09:30:00');
+});
+
+test('tournaround time is greather than the remaining time in the day and cross week-end', () => {
+    expect(calculator.computeDueDate('2025-09-26T12:00:00',16)).toEqual('2025-09-30T12:00:00');
+});
+
+test('touraround time is more than 1 working weeks', () => {
+    expect(calculator.computeDueDate('2025-09-26T09:00:00',55)).toEqual('2025-10-06T16:00:00');
+});
+
+test('tournaround time is less than an hour', () => {
+    expect(calculator.computeDueDate('2025-09-25T09:30:00',0.5)).toEqual('2025-09-25T10:00:00');
+});
+
+test('tournaround time is more than an hour but not a full number of hours', ()=> {
+    expect(calculator.computeDueDate('2025-09-25T09:30:00',2.5)).toEqual('2025-09-25T12:00:00');
+});
+
+test('turnaround greather than remaining hour in the days but less than a day of work', () => {
+    expect(calculator.computeDueDate('2025-09-25T15:25:00',4.75)).toEqual('2025-09-26T12:10:00');
 });
